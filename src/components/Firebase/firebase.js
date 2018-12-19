89442ade6c7865aedd7636b64ff514c980b5464d
Quidiox <33523865+Quidiox@users.noreply.github.com>
2018-12-19 18:55:50 +0200

    before signup with react and firebase

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,32 @@
+import app from 'firebase/app'
+import 'firebase/auth'
+
+const config = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASEURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID
+}
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config)
+    this.auth = app.auth()
+  }
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password)
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password)
+
+  doSignOut = () => this.auth.signOut()
+
+  doPasswordRest = email => this.auth.sendPasswordResetEmail(email)
+
+  doPasswordUpdate = password => this.auth.updatePassword(password)
+}
+
+export default Firebase
